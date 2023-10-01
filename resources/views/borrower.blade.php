23fc2dc9c76293c3e80384e35c449d8fa55f50f0
aj <113114943+aj045045@users.noreply.github.com>
2023-10-01 15:20:48 +0530

    Finishing

--- resources/views/borrower.blade.php
+++ resources/views/borrower.blade.php
@@ -1,10 +1,65 @@
 @extends('./asset/sidenav')
 
 @section('sidepage')
+<style>
+
+    .container {
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 20px;
+    }
+
+    .header {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    .form-group {
+        margin-bottom: 15px;
+    }
+
+    label,
+    input {
+        display: block;
+    }
+
+    input[type="number"] {
+        width: 100%;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+    }
+
+    .btn {
+        background-color: #2196F3;
+    
+        color: rgb(0, 0, 0);
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .btn:hover {
+        background-color: #1976D2;
+      /* Darker blue color on hover */
+    }
+          @media (max-width: 640px) {
+            .sidebar-hidden {
+                left: -64px;
+         
+                transition: left 0.3s ease;
+            }
+
+            .sidebar-show {
+                left: 0;
+                transition: left 0.3s ease;
+            }
+        }
+</style>
 <div class="container">
     <h1 class="header">Peer-to-Peer Lending Platform</h1>
 
-    <div class="borrower-form">
+    <div class="borrower-form p-5  rounded-md mb-5 bg-indigo-300">
         <h2>Borrower</h2>
         <form method="POST" action="">
             <div class="form-group">
--- resources/views/lender.blade.php
+++ resources/views/lender.blade.php
@@ -1,10 +1,66 @@
 @extends('./asset/sidenav')
 @section('sidepage')
+<style>
+    .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .header {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+
+        .form-group {
+            margin-bottom: 15px;
+        }
+
+        label,
+        input {
+            display: block;
+        }
+
+        input[type="number"] {
+            width: 100%;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+        }
+
+        .btn {
+            background-color: #2196F3;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .btn:hover {
+            background-color: #1976D2;
+        }
+
+        /* CSS for Mobile Sidebar Toggle */
+        @media (max-width: 640px) {
+            .sidebar-hidden {
+                left: -64px;
+                /* Adjust the width of your sidebar */
+                transition: left 0.3s ease;
+            }
+
+            .sidebar-show {
+                left: 0;
+                transition: left 0.3s ease;
+            }
+        }
+</style>
         <div class="container">
             <h1 class="header">Peer-to-Peer Lending Platform</h1>
 
             <!-- Lender Form -->
-            <div class="lender-form">
+            <div class="bg-indigo-300  p-5 mb-5 rounded-md">
                 <h2>Lender</h2>
                 <form id="lenderForm">
                     <div class="form-group">
